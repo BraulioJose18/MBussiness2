527e841ea4c03533f3fdc4c2feecbe4568556e0a
BraulioJose18 <brauliochire@gmail.com>
2020-12-21 15:54:42 -0500

    Brands

---
+++ app/src/main/java/com/practica02/mbussiness/fragments/FragmentArticle.java
@@ -0,0 +1,21 @@
+package com.practica02.mbussiness.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.practica02.mbussiness.R;
+
+public class FragmentArticle extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_article,container,false);
+        return view;
+    }
+}
